b1852d44086f8f415a5e20f70294a1e1215c3596
daring-board <mahotox101500@gmail.com>
2018-05-27 15:44:14 +0900

    学習時の処理を変更。

--- furniture/funiture_finetuning.py
+++ furniture/funiture_finetuning.py
@@ -18,7 +18,7 @@ from keras.layers.core import Dense, Activation, Dropout, Flatten
 from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, ProgbarLogger, ReduceLROnPlateau, LambdaCallback
 from keras.layers import Input
 
-from keras.utils import np_utils
+from keras.utils import np_utils, Sequence
 from keras.models import load_model, Model
 from keras.applications.vgg16 import VGG16
 from keras.applications.densenet import DenseNet201
@@ -32,9 +32,9 @@ class FineTuning:
     Avable base_model is
         VGG16, DenseNet201, ResNet50
     '''
-    def __init__(self, train_data, num_classes, base_model):
+    def __init__(self, num_classes, base_model):
         self.num_classes = num_classes
-        self.shape = train_data.shape[1:]
+        self.shape = (128, 128, 3)
         self.input_tensor = Input(shape=self.shape)
         self.base_model = base_model
         if base_model == 'VGG16':
@@ -73,6 +73,46 @@ class FineTuning:
 def join_fn(dat):
     return [dat[0]['image_id'], dat[0]['url'][0], dat[1]['label_id']]
 
+class DataSequence(Sequence):
+    def __init__(self, kind, length, data_path):
+        self.kind = kind
+        self.length = length
+        self.data_file_path = data_path
+        # self.datagen = ImageDataGenerator(horizontal_flip=True, zoom_range=0.5)
+
+    def __getitem__(self, idx):
+        f_list = os.listdir(self.data_file_path)
+        f_list.sort()
+
+        warp = 100
+        aug_time = 2
+        datas, labels = [], []
+
+        for f in random.sample(f_list[idx:], warp):
+            img = cv2.imread(base_path+f)
+            img = cv2.resize(img, (128, 128))
+            img = img.astype(np.float32) / 255.0
+            datas.append(img)
+            tmp_df = train_df[train_df.id == int(f[6:-4])]
+            labels.append(tmp_df['label'].iat[0])
+            # Augmentation image
+            # for num in range(aug_time):
+            #     tmp = datagen.random_transform(img)
+            #     datas.append(tmp)
+            #     labels.append(tmp_df['label'].iat[0])
+
+        datas = np.asarray(datas)
+        labels = pd.DataFrame(labels)
+        labels = np_utils.to_categorical(labels, 129)
+        return datas, labels
+
+    def __len__(self):
+        return self.length
+
+    def on_epoch_end(self):
+        ''' 何もしない'''
+        pass
+
 if __name__=="__main__":
     model = sys.argv[1]
 
@@ -91,57 +131,32 @@ if __name__=="__main__":
     print(f_list[:10])
 
     model_file_name = "funiture_cnn.h5"
-    datagen = ImageDataGenerator(horizontal_flip=True, zoom_range=0.5)
 
-    warp = 10000
-    aug_time = 2
-    n_epoch = 100
-    datas, labels = [], []
+    # モデル構築
+    ft = FineTuning(129, model)
+    model = ft.createNetwork()
+    opt = ft.getOptimizer()
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
 
-    for f in random.sample(f_list, warp):
-        img = cv2.imread(base_path+f)
-        img = cv2.resize(img, (128, 128))
-        img = img.astype(np.float32) / 255.0
-        datas.append(img)
-        tmp_df = train_df[train_df.id == int(f[6:-4])]
-        labels.append(tmp_df['label'].iat[0])
-        # Augmentation image
-        # for num in range(aug_time):
-        #     tmp = datagen.random_transform(img)
-        #     datas.append(tmp)
-        #     labels.append(tmp_df['label'].iat[0])
+    callbacks = [
+        ModelCheckpoint('./checkpoints/weights.%02d.hdf5', verbose=1, save_weights_only=True, monitor='val_loss'),
+        EarlyStopping(monitor='val_loss', patience=3, verbose=0, mode='auto'),
+        ReduceLROnPlateau(factor=0.1, patience=1, verbose=1, cooldown=5, min_lr=1e-10),
+        LambdaCallback(on_batch_begin=lambda batch, logs: print(' now: ',   datetime.datetime.now()))
+    ]
 
-    datas = np.asarray(datas)
-    labels = pd.DataFrame(labels)
-    labels = np_utils.to_categorical(labels, 129)
+    # fit model
+    # model.fit(datas, labels, batch_size=50, epochs=n_epoch, callbacks=callbacks, validation_split=0.1)
+    train_gen = DataSequence('train', 40000, base_path)
+    validate_gen = DataSequence('validate', 4000, base_path)
+    model.fit_generator(
+        train_gen,
+        steps_per_epoch=400,
+        epochs=20,
+        validation_data=validate_gen,
+        validation_steps=40,
+        )
 
-    for iter in range(4):
-        if iter == 0:
-            # モデル構築
-            ft = FineTuning(datas, 129, model)
-            model = ft.createNetwork()
-            opt = ft.getOptimizer()
-            model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-            model.summary()
-        # if iter == 0:
-        #     model = load_model(model_file_name)
-        #     wgt = './checkpoints_vgg16_early/weights.12-00.hdf5'
-        #     model.load_weights(wgt)
-        else:
-            model = load_model(model_file_name)
-            for num in reversed(range(n_epoch)):
-                wgt = './checkpoints/weights.%02d-%02d.hdf5'%(num, iter)
-                if not os.path.isfile(wgt): continue
-                model.load_weights(wgt)
-
-        callbacks = [
-            ModelCheckpoint('./checkpoints/weights.{epoch:02d}-%02d.hdf5'%iter, verbose=1, save_weights_only=True, monitor='val_loss'),
-            EarlyStopping(monitor='val_loss', min_delta=1e-8, patience=3, verbose=0, mode='auto'),
-            ReduceLROnPlateau(factor=0.05, patience=1, verbose=1),
-            LambdaCallback(on_batch_begin=lambda batch, logs: print(' now: ',   datetime.datetime.now()))
-        ]
-
-        # fit model
-        model.fit(datas, labels, batch_size=50, epochs=n_epoch, callbacks=callbacks, validation_split=0.1)
-        # save model
-        model.save(model_file_name)
+    # save model
+    model.save(model_file_name)
